5cc7e5ae8c94ef676a420c1425ff89c52455786f
Josh van Eikeren <josh@vaneikeren.com>
2019-02-06 17:33:00 -0800

    Sample App layout

---
+++ src/Orbital7.RapidApp2.SampleWebApp/Controllers/ModalDialogsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orbital7.RapidApp2.SampleWebApp.Models.ModalDialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orbital7.RapidApp2.SampleWebApp.Controllers
+{
+    public class ModalDialogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new IndexModel());
+        }
+    }
+}
---
+++ src/Orbital7.RapidApp2.SampleWebApp/Models/ModalDialogs/IndexModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orbital7.RapidApp2.SampleWebApp.Models.ModalDialogs
+{
+    public class IndexModel
+    {
+    }
+}
