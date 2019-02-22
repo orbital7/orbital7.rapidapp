d44611c5b485f3cd8711d7b2a0af0d3d84c4b159
Josh van Eikeren <josh@vaneikeren.com>
2019-02-22 11:38:47 -0800

    Modal dialog additions

--- src/Orbital7.RapidApp2.SampleWebApp/Controllers/ModalDialogsController.cs
+++ src/Orbital7.RapidApp2.SampleWebApp/Controllers/ModalDialogsController.cs
@@ -13,5 +13,21 @@ namespace Orbital7.RapidApp2.SampleWebApp.Controllers
         {
             return View(new IndexModel());
         }
+
+        public IActionResult _PanelOverview()
+        {
+            return PartialView(new _PanelOverviewModel());
+        }
+
+        public IActionResult _PanelBasic()
+        {
+            return PartialView(new _PanelBasicModel());
+        }
+
+        public IActionResult _DialogMessage(
+            string message)
+        {
+            return PartialView(_DialogMessageModel.Create(message));
+        }
     }
 }
