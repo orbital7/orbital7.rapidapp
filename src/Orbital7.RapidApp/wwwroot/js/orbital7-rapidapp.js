3ceb64f42dbbf178d5847e3967d04104b7b0d36e
Mark Westbrook <westbrma@gmail.com>
2021-04-15 11:44:11 -0700

    update to date sort matching

--- src/Orbital7.RapidApp/wwwroot/js/orbital7-rapidapp.js
+++ src/Orbital7.RapidApp/wwwroot/js/orbital7-rapidapp.js
@@ -400,7 +400,7 @@ function updateDataGridRowColors(table) {
 
                     item = item.trim();
 
-                    if (item.length > 0) {
+                    if (item.length > 0 && item !== '-') {
                         items.push(item);
                     }
 
@@ -507,11 +507,7 @@ function updateDataGridRowColors(table) {
 // Years can be 4 digits. Days and Months can be 1 or 2 digits.
 (function () {
     Tablesort.extend('date', function (item) {
-        return (
-            item.search(/(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\.?\,?\s*/i) !== -1 ||
-            item.search(/\d{1,2}[\/\-]\d{1,2}[\/\-]\d{2,4}/) !== -1 ||
-            item.search(/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/i) !== -1
-        );
+        return item.search(/\d{1,2}[\/\-]\d{1,2}[\/\-]\d{2,4}/) !== -1
     }, function (a, b) {
         return (Date.parse(b) || -1) - (Date.parse(a) || -1);
     });
