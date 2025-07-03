175cd82f933dacedbc31ca3d68746e963383f119
Josh van Eikeren <jvaneikeren@users.noreply.github.com>
2025-07-02 21:43:12 -0700

    Update to latest Orbital7.Extensions package; add back in unintentionally removed button functionality to RAEditor

--- src/Orbital7.RapidApp/DependencyInjectionExtensions.cs
+++ src/Orbital7.RapidApp/DependencyInjectionExtensions.cs
@@ -2,27 +2,6 @@
 
 public static class DependencyInjectionExtensions
 {
-    // TODO: Move to Orbital7.Extensions.
-    public static IServiceCollection AddDisplayValueOptions(
-        this IServiceCollection services,
-        Action<DisplayValueOptionsBuilder>? configureBuilder = null)
-    {
-        services.AddTransient(provider =>
-        {
-            var builder = new DisplayValueOptionsBuilder();
-            configureBuilder?.Invoke(builder);
-            return builder;
-        });
-
-        services.AddTransient(provider =>
-        {
-            var builder = provider.GetRequiredService<DisplayValueOptionsBuilder>();
-            return builder.Build();
-        });
-
-        return services;
-    }
-
     public static IServiceCollection AddRapidApp(
         this IServiceCollection services,
         Action<DisplayValueOptionsBuilder>? configureDisplayValueOptionsBuilder = null)
---
+++ src/Orbital7.RapidApp/RAAuthenticationStateProviderUserProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Orbital7.RapidApp;
+
+public class RAAuthenticationStateProviderUserProvider :
+    IUserProvider
+{
+    private readonly AuthenticationStateProvider _authenticationStateProvider;
+
+    public RAAuthenticationStateProviderUserProvider(
+        AuthenticationStateProvider authenticationStateProvider)
+    {
+        _authenticationStateProvider = authenticationStateProvider;
+    }
+
+    public async Task<string?> GetCurrentUserIdAsync()
+    {
+        var state = await _authenticationStateProvider.GetAuthenticationStateAsync();
+        return state?.User.GetUserId<string>();
+    }
+}
